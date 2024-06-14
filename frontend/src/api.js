cf2bb4e4940db465fc119866c0172bc55ea41baa
Julian Olarte <julian.olarte@gmail.com>
2024-06-13 21:01:31 -0500

    adding the api key

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -6,9 +6,12 @@ const API_URL = 'https://tf2bfbbm83.execute-api.us-west-2.amazonaws.com/dev/sear
 
 export const fetchHealthProfessionals = async (query) => {
     try {
+        const headers = {
+            'x-api-key': 'K3lS7TYaPI7N6PdYHDSnr5pUWH119tKr5d6NlIc7'
+        }
         const response = await axios.post(API_URL, {
             search: query
-        });
+        }, {headers: headers});
         console.log("original_input: " + response.data.original_input)
         console.log("real_query: " + response.data.real_query)
         return response.data.res.results;
